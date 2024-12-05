9166428c99519d2303eaee2cb23442b5b8ff85ec
vaibhav-chittora <CHITTORAVAIBHAV70@GMAIL.COM>
2024-12-05 22:50:42 +0530

    added userSchema,including avatar using robohash

---
+++ src/schema/user.js
@@ -0,0 +1,44 @@
+import mongoose, { Mongoose } from 'mongoose';
+
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: [true, 'Email already exists'],
+      match: [
+        /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+        'Please fill a valid email address'
+      ]
+    },
+    password: {
+      type: String,
+      required: [true, 'Password is required'],
+      minlength: [8, 'Password must be at least 8 characters long']
+    },
+    username: {
+      type: String,
+      required: [true, 'Username is required'],
+      unique: [true, 'Username already exists'],
+      minlength: [3, 'Username must be at least 3 characters long'],
+      match: [
+        /^[a-zA-Z0-9]+$/,
+        'Username must contain only alphanumeric characters'
+      ]
+    },
+    avatar: {
+      type: String
+    }
+  },
+  { timestamps: true }
+);
+
+userSchema.pre('save', function saveUser(next) {
+  const user = this;
+  user.avatar = `https://robohash.org/${user.username}`;
+  next();
+});
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
