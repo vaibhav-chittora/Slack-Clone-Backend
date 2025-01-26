a315d54ca201ab6b672a5c20952e448c8c85cb15
vaibhav-chittora <CHITTORAVAIBHAV70@GMAIL.COM>
2025-01-26 18:28:41 +0530

    added channel messages using Websockets

--- src/controllers/messageSocketController.js
+++ src/controllers/messageSocketController.js
@@ -7,8 +7,9 @@ import {
 export default function messageHandler(io, socket) {
   socket.on(NEW_MESSAGE_EVENT, async function createMessageHandler(data, cb) {
     const messageResponse = await createMessageService(data);
-    socket.broadcast.emit(NEW_MESSAGE_RECEIVED_EVENT, messageResponse);
-
+    const { channelId } = data;
+    // socket.broadcast.emit(NEW_MESSAGE_RECEIVED_EVENT, messageResponse);
+    io.to(channelId).emit(NEW_MESSAGE_RECEIVED_EVENT, messageResponse);
     cb({
       success: true,
       message: 'Successfully created the message',
