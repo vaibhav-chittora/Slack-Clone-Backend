56e53f7546770849c795a676ea2856c533018619
vaibhav-chittora <CHITTORAVAIBHAV70@GMAIL.COM>
2025-02-24 21:28:29 +0530

    minor validation changes

--- src/validators/workSpaceSchema.js
+++ src/validators/workSpaceSchema.js
@@ -1,7 +1,7 @@
 import { z } from 'zod';
 
 export const createWorkSpaceSchema = z.object({
-  name: z.string().min(3).max(10)
+  name: z.string().min(3)
 });
 
 export const addMemberToWorkSpaceSchema = z.object({
